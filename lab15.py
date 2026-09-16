85aba348ae910e4ed1c1ea58468c266b2833fdd6
riya26-ux <saoriya26@gmail.com>
2026-09-16 10:09:05 +0530

    Add files via upload

---
+++ lab15.py
@@ -0,0 +1,56 @@
+# This is my first python
+import math
+print(math.floor(2.7/2))
+print(math.ceil(2.7/2))
+print(complex(10+5j/2))
+print(2.2+5.3)
+print(round(2.5))
+print(round(-2.5))
+print(2//4-1)
+print(3*2**2)
+print(3**2*2)
+print(7//4)
+print(7%4)
+print(-7%4)
+
+#logical operations
+a=1
+b=2
+c=4
+
+print(a and b)
+print(not a<b or c>b)
+print(type(complex(a,b+c).real) is int)
+#question3
+k=int(input("Enter k:"))
+n=int(input("Enter n:"))
+print ("Addition =", str (k+n))
+print ("substraction =", k-n)
+print ("multiplication =", k*n)
+print ("division =" , k/n)
+
+#question4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
